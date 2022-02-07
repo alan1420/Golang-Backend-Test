6ece23cb4aae3f06a8c96d657a1d1eb68fcf7fb2
alan1420 <alan14@ce.student.pens.ac.id>
2022-02-07 15:21:42 +0700

    first

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+)
+
+const message = "Hello World"
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(message))
+	})
+	http.ListenAndServe(":8080", mux)
+}
